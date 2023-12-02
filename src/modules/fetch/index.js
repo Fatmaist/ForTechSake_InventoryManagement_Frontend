c8f43e59ed1c3c695b84fb072501e8d9239fedca
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-02 20:59:22 +0700

    feat(fetch): add functions to fetch Kategori Barang and Data Supplier

--- src/modules/fetch/index.js
+++ src/modules/fetch/index.js
@@ -1,13 +1,13 @@
 import { instance } from "../axios/index";
 
-// Function for create Data Barang endpoint
-async function createDataBarang(
+// Function to create Data Barang
+async function createDataBarang({
   id_barang,
   nama_barang,
   stok,
   id_kategori,
   id_supplier
-) {
+}) {
   try {
     const response = await instance.post("/data_barang", {
       id_barang,
@@ -22,7 +22,7 @@ async function createDataBarang(
   }
 }
 
-// Function for get all Data Barang endpoint
+// Function to get all Data Barang
 async function getAllDataBarang() {
   try {
     const response = await instance.get("/data_barang");
@@ -32,14 +32,14 @@ async function getAllDataBarang() {
   }
 }
 
-// Function for edit Data Barang endpoint
-async function editDataBarang(
+// Function to edit Data Barang
+async function editDataBarang({
   id_barang,
   nama_barang,
   stok,
   id_kategori,
   id_supplier
-) {
+}) {
   try {
     const response = await instance.put(`/data_barang/${id_barang}`, {
       nama_barang,
@@ -53,7 +53,7 @@ async function editDataBarang(
   }
 }
 
-// Function for delete Data Barang endpoint
+// Function to delete Data Barang
 async function deleteDataBarang(id_barang) {
   try {
     const response = await instance.delete(`/data_barang/${id_barang}`);
@@ -73,10 +73,32 @@ async function getDataBarangDetailById(id_barang) {
   }
 }
 
+// Function to get all Kategori Barang
+async function getAllKategoriBarang() {
+  try {
+    const response = await instance.get("/kategori_barang");
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message || "Something went wrong");
+  }
+}
+
+// Function to get all Data Supplier
+async function getAllDataSupplier() {
+  try {
+    const response = await instance.get("/data_supplier");
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message || "Something went wrong");
+  }
+}
+
 export {
   createDataBarang,
   getAllDataBarang,
   editDataBarang,
   deleteDataBarang,
   getDataBarangDetailById,
+  getAllKategoriBarang,
+  getAllDataSupplier,
 };

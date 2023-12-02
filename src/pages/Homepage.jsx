8bd2b4c7d7eceb6d0089ee4dd5d75b913bb7aa4c
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-02 22:25:11 +0700

    chore : update Homepage component > Refactored and corrected the Homepage component to display accurate data for different categories. Adjusted variable names for clarity.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,24 +1,24 @@
-import { VStack, HStack, Text } from "@chakra-ui/react"
-import { useEffect, useState } from "react"
-import { getAllDataBarang } from "../modules/fetch"
+import { VStack, HStack, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { getAllDataBarang } from "../modules/fetch";
 
 export default function Homepage() {
-  const [dataBarangCount, setDataBarangCount] = useState(0)
+  const [dataBarangCount, setDataBarangCount] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const dataBarang = await getAllDataBarang()
+        const dataBarang = await getAllDataBarang();
 
-        const uniqueIdDataBarang = new Set(dataBarang.map(barang => barang.id_barang))
-        const dataBarangCount = uniqueIdDataBarang.size
-        setDataBarangCount(dataBarangCount)
+        const uniqueIdDataBarang = new Set(dataBarang.map((barang) => barang.id_barang));
+        const uniqueIdDataBarangCount = uniqueIdDataBarang.size;
+        setDataBarangCount(uniqueIdDataBarangCount);
       } catch (error) {
-        console.error("Error fetching data:", error)
+        console.error("Error fetching data:", error);
       }
-    }
-    fetchData()
-  }, [])
+    };
+    fetchData();
+  }, []);
 
   return (
     <VStack width="100%" bg="gray.200" p="4" mb="4">
@@ -28,26 +28,78 @@ export default function Homepage() {
       <Text fontSize="md">Hello, </Text>
       <VStack spacing="16" align="center" height="100vh" justifyContent="center">
         <HStack spacing="20">
-          <VStack bg="green.100" p="4" borderRadius="md" width="320px" height="280px" display="flex" justifyContent="center" alignItems="center">
-            <Text fontSize="xl" fontWeight="bold" textAlign="center">Total Banyak Jenis Data Barang</Text>
-            <Text fontSize="100px" fontWeight="bold">{dataBarangCount}</Text>
+          <VStack
+            bg="green.100"
+            p="4"
+            borderRadius="md"
+            width="320px"
+            height="280px"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text fontSize="xl" fontWeight="bold" textAlign="center">
+              Total Banyak Jenis Data Barang
+            </Text>
+            <Text fontSize="100px" fontWeight="bold">
+              {dataBarangCount}
+            </Text>
           </VStack>
-          <VStack bg="blue.100" p="4" borderRadius="md" width="320px" height="280px" display="flex" justifyContent="center" alignItems="center">
-            <Text fontSize="xl" fontWeight="bold" textAlign="center">Total Permintaan Barang Masuk</Text>
-            <Text fontSize="100px" fontWeight="bold">{dataBarangCount}</Text>
+          <VStack
+            bg="blue.100"
+            p="4"
+            borderRadius="md"
+            width="320px"
+            height="280px"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text fontSize="xl" fontWeight="bold" textAlign="center">
+              Total Permintaan Barang Masuk
+            </Text>
+            <Text fontSize="100px" fontWeight="bold">
+              {dataBarangCount}
+            </Text>
           </VStack>
         </HStack>
         <HStack spacing="20">
-          <VStack bg="red.100" p="4" borderRadius="md" width="320px" height="280px" display="flex" justifyContent="center" alignItems="center">
-            <Text fontSize="xl" fontWeight="bold" textAlign="center">Total Permintaan Barang Keluar</Text>
-            <Text fontSize="100px" fontWeight="bold">{dataBarangCount}</Text>
+          <VStack
+            bg="red.100"
+            p="4"
+            borderRadius="md"
+            width="320px"
+            height="280px"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text fontSize="xl" fontWeight="bold" textAlign="center">
+              Total Permintaan Barang Keluar
+            </Text>
+            <Text fontSize="100px" fontWeight="bold">
+              {dataBarangCount}
+            </Text>
           </VStack>
-          <VStack bg="orange.100" p="4" borderRadius="md" width="320px" height="280px" display="flex" justifyContent="center" alignItems="center">
-            <Text fontSize="xl" fontWeight="bold" textAlign="center">Total Permintaan Restock Barang</Text>
-            <Text fontSize="100px" fontWeight="bold">{dataBarangCount}</Text>
+          <VStack
+            bg="orange.100"
+            p="4"
+            borderRadius="md"
+            width="320px"
+            height="280px"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Text fontSize="xl" fontWeight="bold" textAlign="center">
+              Total Permintaan Restock Barang
+            </Text>
+            <Text fontSize="100px" fontWeight="bold">
+              {dataBarangCount}
+            </Text>
           </VStack>
         </HStack>
       </VStack>
     </VStack>
-  )
+  );
 }

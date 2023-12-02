6d06d852128382daea2da5e38133bcdf5bf71e6c
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-02 21:26:52 +0700

    feat: Enhance DataBarangForm component with dropdown for selecting category and supplier

--- src/components/DataBarangForm.jsx
+++ src/components/DataBarangForm.jsx
@@ -3,24 +3,43 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Select,
   useToast,
   VStack,
-} from "@chakra-ui/react"
-import { useEffect } from "react"
-import { createDataBarang, editDataBarang } from "../modules/fetch"
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { createDataBarang, editDataBarang, getAllDataKategori, getAllDataSupplier } from "../modules/fetch";
 
 export default function DataBarangForm({ data_barang }) {
-  const toast = useToast()
+  const toast = useToast();
+  const [kategoriList, setKategoriList] = useState([]);
+  const [supplierList, setSupplierList] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const kategoriData = await getAllDataKategori();
+        const supplierData = await getAllDataSupplier();
+
+        setKategoriList(kategoriData);
+        setSupplierList(supplierData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   async function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    const formData = new FormData(event.target)
-    const id_barang = parseInt(formData.get("id_barang"))
-    const nama_barang = formData.get("nama_barang").toString()
-    const stok = parseInt(formData.get("stok"))
-    const id_kategori = parseInt(formData.get("id_kategori"))
-    const id_supplier = parseInt(formData.get("id_supplier"))
+    const formData = new FormData(event.target);
+    const id_barang = parseInt(formData.get("id_barang"));
+    const nama_barang = formData.get("nama_barang").toString();
+    const stok = parseInt(formData.get("stok"));
+    const id_kategori = parseInt(formData.get("id_kategori"));
+    const id_supplier = parseInt(formData.get("id_supplier"));
 
     if (data_barang) {
       try {
@@ -30,7 +49,7 @@ export default function DataBarangForm({ data_barang }) {
           stok,
           id_kategori,
           id_supplier
-        )
+        );
 
         toast({
           title: "Success",
@@ -38,7 +57,7 @@ export default function DataBarangForm({ data_barang }) {
           status: "success",
           duration: 5000,
           isClosable: true,
-        })
+        });
       } catch (error) {
         toast({
           title: "Error",
@@ -46,9 +65,9 @@ export default function DataBarangForm({ data_barang }) {
           status: "error",
           duration: 5000,
           isClosable: true,
-        })
+        });
       }
-      return
+      return;
     }
 
     try {
@@ -58,16 +77,16 @@ export default function DataBarangForm({ data_barang }) {
         stok,
         id_kategori,
         id_supplier,
-      })
+      });
 
-      event.target.reset()
+      event.target.reset();
       toast({
         title: "Success",
         description: "Data Barang created successfully",
         status: "success",
         duration: 5000,
         isClosable: true,
-      })
+      });
     } catch (error) {
       toast({
         title: "Error",
@@ -75,7 +94,7 @@ export default function DataBarangForm({ data_barang }) {
         status: "error",
         duration: 5000,
         isClosable: true,
-      })
+      });
     }
   }
 
@@ -110,27 +129,37 @@ export default function DataBarangForm({ data_barang }) {
           />
         </FormControl>
         <FormControl>
-          <FormLabel>ID Kategori</FormLabel>
-          <Input
+          <FormLabel>Kategori</FormLabel>
+          <Select
             name="id_kategori"
-            type="number"
-            required
             defaultValue={data_barang?.id_kategori}
-          />
+            required
+          >
+            {kategoriList.map((kategori) => (
+              <option key={kategori.id_kategori} value={kategori.id_kategori}>
+                {kategori.nama_kategori}
+              </option>
+            ))}
+          </Select>
         </FormControl>
         <FormControl>
-          <FormLabel>ID Supplier</FormLabel>
-          <Input
+          <FormLabel>Supplier</FormLabel>
+          <Select
             name="id_supplier"
-            type="number"
-            required
             defaultValue={data_barang?.id_supplier}
-          />
+            required
+          >
+            {supplierList.map((supplier) => (
+              <option key={supplier.id_supplier} value={supplier.id_supplier}>
+                {supplier.nama_supplier}
+              </option>
+            ))}
+          </Select>
         </FormControl>
         <Button type="submit">
           {data_barang ? "Edit Data Barang" : "Create Data Barang"}
         </Button>
       </VStack>
     </form>
-  )
+  );
 }
